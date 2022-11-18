19ed139e028bfcfcc842badd9c45c6467ccbc956
Victor <victorlimasantos.dev@gmail.com>
2022-11-18 10:40:51 -0300

    Criado Jogo de cartas 21

---
+++ Jogos/Simulador_de_Dados.py
@@ -0,0 +1,15 @@
+import random
+
+print("**************************************")
+print("***Bem vindo ao simulador de DADOS!***")
+print("**************************************")
+
+qtd_dados = int(input("quantos dados deseja adicionar?"))
+qtd_faces = int(input("seram dados de quantas faces?"))
+#qtd_rodadas = int(input("quantas vezes vai rodar os dados?"))
+
+
+
+
+
+
